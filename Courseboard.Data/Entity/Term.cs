d0a1edc695f383f6892602bb582273f995b5076c
Samantha Hsu <shsu877@gmail.com>
2021-04-06 22:38:05 -0700

    add formatting to main window

--- Courseboard.Data/Entity/Term.cs
+++ Courseboard.Data/Entity/Term.cs
@@ -6,10 +6,8 @@ namespace Courseboard.Data
 {
     public class Term
     {
-
         public int TermId { get; set; }
         public string Name { get; set; }
-        public IEnumerable<Course> Courses { get; set; }
         public string Notes { get; set; }
     }
 }
---
+++ Courseboard.Data/SQLiteDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Courseboard.Data
+{
+    public class SQLiteDataAccess
+    {
+    }
+}
